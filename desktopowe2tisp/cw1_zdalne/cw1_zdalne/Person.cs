5ff465d1002800560c8ab49cb8119db3720c83aa
pszczesz <pszczesz@gmail.com>
2020-10-19 09:03:52 +0200

    dd
    dd

---
+++ desktopowe2tisp/cw1_zdalne/cw1_zdalne/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw1_zdalne
+{
+    class Person {
+        private string name;
+        private int age;
+        private string lastName;
+
+        public Person() {
+            name = "noname";
+            age = 20;
+            lastName = "noname";
+        }
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age {
+            get { return age; }
+            set { age = value < 0 || value > 120 ? 20 : value; }
+        }
+
+        public Person(string name,string lastName, int age) {
+            this.name = name;
+            this.age = age;
+            this.lastName = lastName;
+        }
+
+        public override string ToString() {
+            return "To jest osoba: " + name + " w wieku: " + age;
+        }
+    }
+}
---
+++ desktopowe2tisp/cw1_zdalne/cw1_zdalne/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw1_zdalne
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Ex1();
+            Console.ReadKey();
+        }
+
+        public static void Ex1() {
+            Person p1 = new Person("Antoni",33);
+            p1.Name = "Tomasz";
+            p1.Age = -4;
+            Console.WriteLine(p1);
+        }
+    }
+}
