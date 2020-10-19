7afc65758bf34fbc3d51f7aea5830bb01fd5868c
pszczesz <pszczesz@gmail.com>
2020-10-19 11:50:16 +0200

    ff

---
+++ webowe2tiGim/Cw2Empty/Pages/Nowa1.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class Nowa1Model : PageModel
+    {
+        public string[] Kwiaty{get;set;}
+        public void OnGet()
+        {
+            Kwiaty = new string[5];
+            fillKwiaty();
+        }
+        private void fillKwiaty(){
+            if(Kwiaty == null) Kwiaty = new string[5];
+            Kwiaty[0] = "Róża";
+            Kwiaty[1] = "Goździk";
+            Kwiaty[2] = "Stokrotka";
+            Kwiaty[3] = "Tulipan";
+            Kwiaty[4] = "Przebiśniegi";
+        }
+    }
+}
