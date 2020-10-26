66239a1f0d6a2028a16965bd0bc008993fa51045
pszczesz <pszczesz@gmail.com>
2020-10-26 11:48:05 +0100

    dd

--- webowe2tiGim/Cw2Empty/Pages/Nowa1.cshtml.cs
+++ webowe2tiGim/Cw2Empty/Pages/Nowa1.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyApp.Namespace
+namespace Cw2Empty
 {
     public class Nowa1Model : PageModel
     {
--- webowe2tiGim/Cw2Empty/Pages/Nowa2.cshtml.cs
+++ webowe2tiGim/Cw2Empty/Pages/Nowa2.cshtml.cs
@@ -5,12 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyApp.Namespace
+namespace Cw2Empty
 {
     public class Nowa2Model : PageModel
     {
-        public void OnGet()
-        {
+        public int? A { get; set; }
+        public int? B { get; set; }
+        public void OnGet() {
+            A = null;
+            B = null;
+        }
+
+        public void OnPost() {
+            A = Convert.ToInt32(Request.Form["a"]);
+            B = Convert.ToInt32(Request.Form["b"]);
         }
     }
 }
