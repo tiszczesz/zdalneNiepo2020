b150ac6cc881b441dd0bb2fdc5ff6f4b0c77cecd
pszczesz <pszczesz@gmail.com>
2020-10-27 16:52:37 +0100

    dd
    dd

--- webowe2tiSP/Cw1Empty/Pages/Nowa2.cshtml.cs
+++ webowe2tiSP/Cw1Empty/Pages/Nowa2.cshtml.cs
@@ -11,12 +11,24 @@ namespace MyApp.Namespace
     {
         public int A { get; set; }
         public int B { get; set; }
-        public void OnGet()
-        {
+        public bool IsPost { get; set; }
+        public void OnGet() {
+            IsPost = false;
         }
 
         public void OnPost() {
-            A = Convert.ToInt32(Request.Form["A"]);
+            IsPost = true;
+            try {
+                A = Convert.ToInt32(Request.Form["A"]);
+                B = Convert.ToInt32(Request.Form["B"]);
+            }
+            catch (System.FormatException ex) {
+                A = 20;
+                B = 20;
+            }
+
+            A = A < 1 || A > 50 ? 20 : A;
+            B = B < 1 || B > 50 ? 20 : B;
         }
     }
 }
