00c9ab10f0926dc9303011a5e00349d2d29de5c3
pszczesz <pszczesz@gmail.com>
2020-11-04 13:22:37 +0100

    dd

--- szkola2020/2tiGim/cw3/cw3.php
+++ szkola2020/2tiGim/cw3/cw3.php
@@ -8,15 +8,15 @@
 <body>
     <h1>Operacje na plikach</h1>
     <?php
-    $plik = fopen("dane.txt","r");
+    $plik = fopen("dane.txt","r");// r r+ w w+ a a+
    // var_dump($plik);
     if($plik){
-        while($line = fgets($plik)){
+        while($line = fgets($plik)){  //fgetc ...
             echo "<div>{$line}</div>";
         }
         fclose($plik);
     }
-    $plik = fopen("wynik.txt",'a');
+    $plik = fopen("wynik.txt",'w');
     if($plik){
         fwrite($plik,"Ala ma kota".PHP_EOL);
         fclose($plik);
