b881097b5bf35b9b04b920177c46dbbfb2180cde
pszczesz <pszczesz@gmail.com>
2020-11-16 11:42:49 +0100

    ff

--- webowe2tiGim/WebEmptyVS2019RazorPages/WebEmptyVS2019RazorPages/Pages/Bmi.cshtml.cs
+++ webowe2tiGim/WebEmptyVS2019RazorPages/WebEmptyVS2019RazorPages/Pages/Bmi.cshtml.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
+using System.Globalization;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -14,18 +16,31 @@ namespace WebEmptyVS2019RazorPages.Pages
         public double Wzrost { get; set; }
         public double BMI { get; set; }
         public bool IsPost { get; set; }
+        public string Plec { get; set; }
         public void OnGet() {
             IsPost = false;
         }
 
         public void OnPost() {
             IsPost = true;
-            var plec = Request.Form["plec"];
+            Masa = Convert.ToDouble(Request.Form["masa"], CultureInfo.InvariantCulture);
+            Wzrost = Convert.ToDouble(Request.Form["wzrost"], CultureInfo.InvariantCulture);
+            Plec = Request.Form["plec"];
+            //var plec = Request.Form["plec"];
+            Wzrost = Wzrost > 3 ? Wzrost / 100 : Wzrost;
+            double roznica = Plec == "Kobieta" ? 2 : 0;
+            BMI = Math.Round(Masa / (Wzrost * Wzrost),2)-roznica;
         }
         public string Description() {
-            //if(BMI<16) return "wyg³odzenie";
-            // else if(BMI >=16 && BMI <16.99) return "wychudzenie";
-            return "";
+            if(BMI<16) return "wyg³odzenie";
+            if (BMI >= 16 && BMI < 17) return "Wychdzuenie";
+            if (BMI >= 17 && BMI < 18.5) return "niedowaga";
+            if (BMI >= 18.5 && BMI < 25) return "wartoœæ prawid³owa";
+            if (BMI >= 25 && BMI < 30) return "nadwaga";
+            if (BMI >= 30 && BMI < 35) return "I stopieñ oty³oœci";
+            if (BMI >= 35 && BMI < 40) return "II stopieñ oty³oœci";
+            else return "oty³oœæ skrajna";
+            
         }
     }
 }
