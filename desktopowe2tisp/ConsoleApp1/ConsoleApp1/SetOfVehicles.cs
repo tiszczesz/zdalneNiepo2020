47d9a562b346006f74e31a39731e332f9eaad7be
pszczesz <pszczesz@gmail.com>
2020-11-16 09:11:58 +0100

    dd

---
+++ desktopowe2tisp/ConsoleApp1/ConsoleApp1/SetOfVehicles.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class SetOfVehicles
+    {
+        public List<Vehicle> Vehicles { get; set; }
+
+        private void fillCars() {
+            if(Vehicles==null) Vehicles = new List<Vehicle>();
+            Vehicles.Add(new Vehicle(){Name = "Fiat 125p",Mass=1200,MaxSpeed=140});
+            Vehicles.Add(new Vehicle() { Name = "Fiat 126p", Mass = 700, MaxSpeed = 120 });
+            Vehicles.Add(new Vehicle() { Name = "Renaut Clio", Mass = 1100, MaxSpeed = 220 });
+            Vehicles.Add(new Vehicle() { Name = "Polonez", Mass = 1500, MaxSpeed = 140 });
+        }
+
+        public SetOfVehicles() {
+            fillCars();
+        }
+
+        public void ShowAll() {
+            foreach (var car in Vehicles) {
+                Console.WriteLine(car.ShowInfo());
+            }
+        }
+
+        public void AddNewCar() {
+            //todo
+        }
+    }
+}
---
+++ desktopowe2tisp/ConsoleApp1/ConsoleApp1/Vehicle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Vehicle
+    {
+        public string Name { get; set; }
+        private double maxSpeed;
+
+        public double MaxSpeed {
+            get { return maxSpeed; }
+            set { maxSpeed = value < 0 ? -value : value; }
+        }
+
+        private double mass;
+        public double Mass {
+            get { return mass; }
+            set { mass = value < 0 ? -value : value; }
+        }
+
+        public Vehicle() {
+            Mass = 1000;
+            MaxSpeed = 120;
+            Name = "Noname";
+        }
+
+        public Vehicle(string name,double maxSpeed, double mass) {
+            Name = name;
+            this.maxSpeed = maxSpeed;
+            this.mass = mass;
+        }
+
+        public string ShowInfo() {
+            return $" Informacje o pojezdzie: {Name} masa: {Mass} kg maksymalna predkosc: {MaxSpeed} km/h";
+        }
+    }
+}
