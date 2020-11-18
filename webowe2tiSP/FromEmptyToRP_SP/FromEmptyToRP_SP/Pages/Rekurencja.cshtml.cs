040356c22cace25b85084b74594f546142e09c83
pszczesz <pszczesz@gmail.com>
2020-11-18 10:29:27 +0100

    dd
    dd

---
+++ webowe2tiSP/FromEmptyToRP_SP/FromEmptyToRP_SP/Pages/Rekurencja.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FromEmptyToRP_SP.Pages
+{
+    public class RekeruncjaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
