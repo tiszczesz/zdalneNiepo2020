fd3ea2bc55ab9264c60a80ea13db5863bc2758b9
pszczesz <pszczesz@gmail.com>
2020-11-18 08:42:24 +0100

    gg

--- szkola2020/2tiGim/cw5/cw5.php
+++ szkola2020/2tiGim/cw5/cw5.php
@@ -7,16 +7,19 @@
     <title>Document</title>
 </head>
 
-<body>    
+<body>
     <div>
         <?php
         require "functions.php";
         $tekst = "Alą ma kołta";
-        echo InsertIntoText($tekst, ' ');
-
+        echo InsertIntoText($tekst, '-');
+        echo "<br>";
+        echo InsertIntoText("sdsfsfsfsfsfs",'-');
+        echo "<br>";
+        echo InsertIntoText("Bardzo Fajny długi tekst do sprawdzenia łódka",'_');
         ?>
     </div>
-   
+
 </body>
 
 </html>
--- szkola2020/2tiGim/cw5/functions.php
+++ szkola2020/2tiGim/cw5/functions.php
@@ -1,8 +1,16 @@
 <?php
-function InsertIntoText(string $text,string $char):string{
+function InsertIntoText(string $text, string $char): string
+{
     $result = "";
-    for($i=0; $i < strlen($text);$i++){
-        $result .= $text[$i].$char;
+    $wyrazy = explode(" ", $text);   
+    
+    foreach ($wyrazy as $wyraz) {      
+        for ($i = 0; $i < mb_strlen($wyraz); $i++) {
+            $t = $i < (mb_strlen($wyraz) - 1) ? $char : "";
+            $result .= mb_substr($wyraz, $i, 1) . $t;
+        }
+        $result.=" ";
     }
+
     return $result;
 }
