e2bd676c7d90b93b592ce1188177417aa7e7e0b0
Paweł Szczeszek <pszczesz@gmail.com>
2020-12-03 21:48:10 +0100

    dd

---
+++ zzz/con1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace con1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //  SaveToFile();
+      GetFromDB();
+      // Console.ReadKey();
+    }
+    public static void SaveToFile()
+    {
+      string[] lines = { "ala ma kota", "żaba jest żółta", "rybki śpią w jeziorze","1111111" };
+      string path = Environment.CurrentDirectory;
+      using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, "dane.txt"), false))
+      {
+        foreach (string line in lines)
+        {
+          outputFile.WriteLine(line);
+        }
+      }
+      Console.WriteLine(path);
+    }
+
+    public static void GetFromDB() {
+        
+        string connectionString =
+            @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=RadioButtonListDemo;Integrated Security=True;";
+        SqlConnection conn = new SqlConnection(connectionString);
+        conn.Open();
+        SqlCommand command = conn.CreateCommand();
+        command.CommandText = "SELECT * FROM Category";
+        SqlDataReader sr = command.ExecuteReader();
+        while (sr.Read()) {
+            Console.WriteLine(sr.GetValue(1));
+        } 
+        conn.Close();
+    }
+  }
+}
