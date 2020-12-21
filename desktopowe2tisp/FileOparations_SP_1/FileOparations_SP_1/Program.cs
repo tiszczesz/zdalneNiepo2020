bd5a447576893c87c8a8c415283053dd6fd7158f
pszczesz <pszczesz@gmail.com>
2020-12-21 08:40:39 +0100

    dd

--- desktopowe2tisp/FileOparations_SP_1/FileOparations_SP_1/Program.cs
+++ desktopowe2tisp/FileOparations_SP_1/FileOparations_SP_1/Program.cs
@@ -7,7 +7,9 @@ namespace FileOparations_SP_1
     class Program
     {
         static void Main(string[] args) {
-            Ex1();
+            //Ex1();
+           // Ex2();
+           Ex3();
             Console.ReadKey();
         }
 
@@ -26,6 +28,41 @@ namespace FileOparations_SP_1
             foreach (string s in dane) {
                 Console.WriteLine(s);
             }
+        }
+
+        public static void Ex2() {
+            string text =
+                "Tekst ktory zapiszemy do pliku\nTekst ten jest bez konkretnej tresci\ntakie zwykle lorem ipsum\n"
+                     +"blblbblblblb";
+            File.WriteAllText("dane2.txt",text);
+            string result = File.ReadAllText("dane2.txt");
+            Console.WriteLine(result);
+        }
+
+        public static void Ex3() {
+            List<string> lista = new List<string> {
+                "Ala ma kota",
+                "Juz za chwile swieta",
+                "I jeszcze potem ferie",
+                "zostanie tylko zasuwanie do samych swiat wielkanocnych",
+                "nowy tekst",
+                "To wszystko zapisane za pomoca StreamWriter'a"
+            };
+            int count = 0;
+            string line;
+            StreamWriter sw = new StreamWriter("dane3.txt");
+            foreach (string row in lista) {
+                sw.WriteLine(row);
+            }
+            sw.Close();
+            Console.WriteLine("Zapisano do pliku");
+            using (StreamReader sr = new StreamReader("dane3.txt")) {
+                while ((line = sr.ReadLine())!=null) {
+                    count++;
+                    Console.WriteLine($"Linia tekstu nr:{count} - {line}");
+                }
+            }
+            Console.WriteLine("Odczytano z pliku");
         }
     }
 }
