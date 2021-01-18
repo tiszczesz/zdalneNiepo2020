a093817d6421b1ae44dd8ffa645e24628892b215
pszczesz <pszczesz@gmail.com>
2021-01-18 10:52:08 +0100

    dd

--- webowe2tiGim/RazorPageWithAdoNet_Gim/RazorPageWithAdoNet_Gim/Pages/Index.cshtml.cs
+++ webowe2tiGim/RazorPageWithAdoNet_Gim/RazorPageWithAdoNet_Gim/Pages/Index.cshtml.cs
@@ -3,15 +3,17 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RazorPageWithAdoNet_Gim.Pages
 {
-    public class IndexModel : PageModel
-    {
+    public class IndexModel : PageModel {
+        private string sqlConnectionString =
+            @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Filmy2020_Gim_cw1;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         private readonly ILogger<IndexModel> _logger;
-
+        private SqlConnection connection;
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -19,7 +21,9 @@ namespace RazorPageWithAdoNet_Gim.Pages
 
         public void OnGet()
         {
-
+            connection = new SqlConnection(sqlConnectionString);
+            connection.Open();
+            var state = connection.State;
         }
     }
 }
