f32210405814fb8925f46dceff5d309c9d7ae188
pszczesz <pszczesz@gmail.com>
2021-01-25 10:51:01 +0100

    dd

---
+++ webowe2tiGim/RazorPageWithAdoNet_Gim/RazorPageWithAdoNet_Gim/Models/Film.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPageWithAdoNet_Gim.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Length { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- webowe2tiGim/RazorPageWithAdoNet_Gim/RazorPageWithAdoNet_Gim/Pages/Index.cshtml.cs
+++ webowe2tiGim/RazorPageWithAdoNet_Gim/RazorPageWithAdoNet_Gim/Pages/Index.cshtml.cs
@@ -3,13 +3,19 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using RazorPageWithAdoNet_Gim.Models;
 
 namespace RazorPageWithAdoNet_Gim.Pages
 {
     public class IndexModel : PageModel {
+
+        [BindProperty]
+        public List<Film> Films { get; set; }
+
         private string sqlConnectionString =
             @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Filmy2020_Gim_cw1;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         private readonly ILogger<IndexModel> _logger;
@@ -22,8 +28,25 @@ namespace RazorPageWithAdoNet_Gim.Pages
         public void OnGet()
         {
             connection = new SqlConnection(sqlConnectionString);
+            Films = getAllFilms(connection);
+        }
+
+        private List<Film> getAllFilms(SqlConnection connection) {
+            List<Film> films = new List<Film>();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "select Id,Title,Author,Length,Price from Filmy";
             connection.Open();
-            var state = connection.State;
+            SqlDataReader dr = command.ExecuteReader();
+            if (dr.HasRows) {
+                while (dr.Read()) {
+                    films.Add(new Film {
+                        Id = dr.GetInt32(0), Title = dr.GetString(1), Author = dr.GetString(2),
+                        Length = dr.GetInt32(3), Price = dr.GetDecimal(4)
+                    });
+                }
+            }
+            connection.Close();
+            return films;
         }
     }
 }
