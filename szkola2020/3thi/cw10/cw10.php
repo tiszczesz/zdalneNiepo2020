8ae6ef9900ac9418d329a6b115cc8b0a65aacb5b
pszczesz <pszczesz@gmail.com>
2021-01-26 15:00:37 +0100

    dd

--- szkola2020/3thi/cw10/cw10.php
+++ szkola2020/3thi/cw10/cw10.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+    .game-info{padding: 8px; color: green; background-color: lightgreen;}
+    </style>
 </head>
 <body>
     <?php
@@ -20,17 +23,39 @@
         public function setTitle(string $title):void{
             $this->title = $title;
         }
+        public function setPrice(float $price):void 
+        {
+            $this->price = $price < 0 ? -$price : $price;
+        }
+        public function setCategory(string $category):void 
+        {
+           $this->category = $category;
+        }
+        public function __toString():string
+        {
+           return "<div class='game-info'><b>{$this->title}</b> gatunek: <b>{$this->category}</b> cena: <i>{$this->price}</i> zł</div>";
+        }
+        public function __construct(string $title="noname",string $category="noname",float $price=0) {
+            echo "<h3>Wywołanie konstruktora!!!!</h3>";
+            $this->setTitle($title);
+            $this->setCategory($category);
+            $this->setPrice($price);
+        }
+      
     }
-    $g1 = new Game();
+    //todo Klasa Article z polami content, category, date, style
+    $g1 = new Game("Worms","Fajny",12.99);
     $g2 = new Game();
     $g3 = new Game();
     $g3->setTitle("Fajna 3 gra!!");
-    $g3->price = 45.90;
+    $g3->setPrice(45.90);
+    echo $g1;
+    echo $g2->Info();
+    echo $g3->Info();
+    $g3->setCategory("Co za RPG") ;
 
     echo $g3->Info();
-    $g3->category = "Co za RPG";
-
-    echo $g3->Info();
+    echo "<div>{$g3}</div>";
     
     ?>
 </body>
