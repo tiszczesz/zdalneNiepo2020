d603c091fa4b3855088ad6257a091b8026e4a87a
pszczesz <pszczesz@gmail.com>
2021-02-03 10:26:04 +0100

    dd

--- webowe2tiSP/RazorPagesADO_Net1/RazorPagesADO_Net1/Models/Game.cs
+++ webowe2tiSP/RazorPagesADO_Net1/RazorPagesADO_Net1/Models/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,18 @@ namespace RazorPagesADO_Net1.Models
     public class Game
     {
         public int Id { get; set; }
+        
+        [Display(Name = "Tytuł gry")]
+        [Required(ErrorMessage = "Musisz podać tytuł")]
         public string Title { get; set; }
+        
+        [Display(Name = "Kategoria gry")]
         public string Category { get; set; }
+        
+        [Display(Name = "Cena")]
         public decimal Price { get; set; }
+        
+        [Display(Name = "Opis gry")]
         public string Description { get; set; }
     }
 }
--- webowe2tiSP/RazorPagesADO_Net1/RazorPagesADO_Net1/Pages/GameIndex.cshtml.cs
+++ webowe2tiSP/RazorPagesADO_Net1/RazorPagesADO_Net1/Pages/GameIndex.cshtml.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using RazorPagesADO_Net1.Models;
+
 
 namespace RazorPagesADO_Net1.Pages
 {
-    public class GameIndexModel : PageModel
-    {
+    public class GameIndexModel : PageModel {
+        private string connString;
+        private IConfiguration configuration;
+
+        [BindProperty]
+        public Game MyGame { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public GameIndexModel(IConfiguration configuration) {
+            this.configuration = configuration;
+            connString = configuration["ConnectionStrings:DefaultConnection"];
         }
     }
 }
