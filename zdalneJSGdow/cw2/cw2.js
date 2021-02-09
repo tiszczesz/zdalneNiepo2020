869ed61a197ac21656befa4b276b79e54cbdea21
pszczesz <pszczesz@gmail.com>
2021-02-09 09:04:36 +0100

    gg

---
+++ zdalneJSGdow/cw2/cw2.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",function(){
+    alert("dddddd");
+   let elem1 = document.querySelector("#ex1");
+   elem1.style.color = "red";
+    console.log(elem1);
+});
