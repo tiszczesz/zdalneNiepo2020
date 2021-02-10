4877ee3fdcf44d52a63cc93d99997fbc33b773dc
pszczesz <pszczesz@gmail.com>
2021-02-10 10:22:45 +0100

    hh

--- webowe2tiSP/RazorPagesADO_Net1/RazorPagesADO_Net1/Pages/GameIndex.cshtml.cs
+++ webowe2tiSP/RazorPagesADO_Net1/RazorPagesADO_Net1/Pages/GameIndex.cshtml.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -31,11 +33,19 @@ namespace RazorPagesADO_Net1.Pages
             if (ModelState.IsValid) {
                 using (SqlConnection connection = new SqlConnection(connString)) {
                     string sql =
-                        $"INSERT INTO Games(Title,Category,Price,Description ) VALUES('{MyGame.Title}','{MyGame.Category}',{MyGame.Price},'{MyGame.Description}')";
+                        $"INSERT INTO Games(Title,Category,Price,Description ) VALUES('{MyGame.Title}','{MyGame.Category}',{MyGame.Price.ToString(new CultureInfo("en_US"))},'{MyGame.Description}')";
                     using (SqlCommand command = new SqlCommand(sql,connection)) {
-                        
+                        command.CommandType = CommandType.Text;
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
                     }
                 }
+
+                ViewData["info"] = "Zapisano grê do bazy danych";
+            }
+            else {
+                ViewData["info"] = "B³êdne dane!!!";
             }
             
         }
