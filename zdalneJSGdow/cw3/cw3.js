42a186a8b17b253804f7f32cf88208a14a074a14
pszczesz <pszczesz@gmail.com>
2021-02-12 12:38:48 +0100

    dd

---
+++ zdalneJSGdow/cw3/cw3.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    document.querySelector("#first").addEventListener("click",function(){
+        alert(this.innerHTML);
+    });
+
+
+
+});
