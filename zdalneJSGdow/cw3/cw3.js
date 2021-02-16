596f898365638aaa029f9348fd64249a29f45ced
pszczesz <pszczesz@gmail.com>
2021-02-16 08:44:50 +0100

    dd

--- zdalneJSGdow/cw3/cw3.js
+++ zdalneJSGdow/cw3/cw3.js
@@ -4,6 +4,28 @@ document.addEventListener("DOMContentLoaded", function(){
         alert(this.innerHTML);
     });
 
+    document.querySelector("#last").addEventListener("click",function(){
+        console.log(this);
+        if(this.style.color!="red") this.style.color = "red";
+        else this.style.color = "black";
+    });
+    //let last = document.querySelector("#last");
+   // if(last!==null){
+    //    last.addEventListener("click",function(){});
+   // }
 
-
+    let pars = document.querySelectorAll("p");
+    // for(let i=0;i<pars.length;i++){
+    //     console.log(pars[i].innerHTML);
+    // }
+    for(let p of pars){
+        p.addEventListener("click",function(){
+            if(this.style.backgroundColor!="yellow") this.style.backgroundColor="yellow";
+            else this.style.backgroundColor="white";
+        });
+    }
+    pars.forEach(function(a,b){
+        console.log(a,b);
+    })
+   // console.log(pars);
 });
