9a41c99d6e81ebd6dc77d5981c50f92f09616dc1
pszczesz <pszczesz@gmail.com>
2021-02-23 10:46:31 +0100

    ss

--- zdalneJSGdow/cw7/cw7.js
+++ zdalneJSGdow/cw7/cw7.js
@@ -5,6 +5,30 @@ document.addEventListener("DOMContentLoaded",function(){
         console.log(this,this.value);
         event.target.value = event.target.value.toUpperCase();
     });
+    document.querySelector("#rr1").addEventListener("change",function(e){
+        console.log(e.target);
+        let wynik = document.querySelector("#wynik");
+        if(wynik.style.visibility=="hidden"){
+            wynik.style.visibility="visible";
+        }else{
+            wynik.style.visibility="hidden";
+        }
+    });
+    document.querySelector("#red").addEventListener("change",function(e){
+        console.log(e.target);
+        let wynik = document.querySelector("#wynik");
+        wynik.style.backgroundColor = `rgb(${e.target.value},255,255)`;
+    });
+    document.querySelector("#green").addEventListener("change",function(e){
+        console.log(e.target);
+        let wynik = document.querySelector("#wynik");
+        wynik.style.backgroundColor = `rgb(255,${e.target.value},255)`;
+    });
+    document.querySelector("#blue").addEventListener("change",function(e){
+        console.log(e.target);
+        let wynik = document.querySelector("#wynik");
+        wynik.style.backgroundColor = `rgb(255,255,${e.target.value})`;
+    });
     //dodac input tekst na imie i nazwisko i przycisk
     //przy zdarzeniu change zamieni wszystkie litery na duze
 });
