22b6ff3ecd9d1d640179e1da1c19b299eb80e323
pszczesz <pszczesz@gmail.com>
2021-03-02 14:09:12 +0100

    dd

--- webowe2tiSP/RazorPagesADO_Net1/RazorPagesADO_Net1/Pages/Edit.cshtml.cs
+++ webowe2tiSP/RazorPagesADO_Net1/RazorPagesADO_Net1/Pages/Edit.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Globalization;
 using System.Linq;
@@ -29,8 +30,14 @@ namespace RazorPagesADO_Net1.Pages
             if (id == null) return RedirectToPage("Index");
             using (SqlConnection connection = new SqlConnection(connString)) {
                 using (SqlCommand command = connection.CreateCommand()) {
-                    string sql = $"Select Id,Title,Category,Price,Description From Games WHERE Id={id}";
+                    string sql = "Select Id,Title,Category,Price,Description From Games WHERE Id=@id";
                     command.CommandText = sql;
+                    SqlParameter param = new SqlParameter {
+                        ParameterName = "@id",
+                        Value = id,
+                        SqlDbType = SqlDbType.Int
+                    };
+                    command.Parameters.Add(param);
                     connection.Open();
                     using (SqlDataReader dr = command.ExecuteReader()) {
                         if (dr.HasRows) {
@@ -56,10 +63,43 @@ namespace RazorPagesADO_Net1.Pages
         public IActionResult OnPost(int id) {
             if (ModelState.IsValid) {
                 using (SqlConnection connection = new SqlConnection(connString)) {
-                    string sql = $"UPDATE Games SET Title='{MyGame.Title}',Category='{MyGame.Category}',"
-                                 + $"Price='{MyGame.Price.ToString(new CultureInfo("en-US"))}',"
-                                 + $"Description='{MyGame.Description}' WHERE Id={id}";
+                    string sql = "UPDATE Games SET Title=@Title,Category=@Category,"
+                                 + "Price=@Price,"
+                                 + "Description=@Description WHERE Id=@id";
                     using (SqlCommand command = new SqlCommand(sql,connection)) {
+                        SqlParameter param1 = new SqlParameter {
+                            ParameterName = "@Title",
+                            Value = MyGame.Title,
+                            SqlDbType = SqlDbType.NVarChar,
+                            Size = 50
+                        };
+                        command.Parameters.Add(param1);
+                        SqlParameter param2 = new SqlParameter {
+                            ParameterName = "@Category",
+                            Value = MyGame.Category,
+                            SqlDbType = SqlDbType.NVarChar,
+                            Size = 50
+                        };
+                        command.Parameters.Add(param2);
+                        SqlParameter param3 = new SqlParameter {
+                            ParameterName = "@Price",
+                            Value = MyGame.Price,
+                            SqlDbType = SqlDbType.Decimal,
+                        };
+                        command.Parameters.Add(param3);
+                        SqlParameter param4 = new SqlParameter {
+                            ParameterName = "@Description",
+                            Value = MyGame.Description,
+                            SqlDbType = SqlDbType.NVarChar,
+                            Size = 100
+                        };
+                        command.Parameters.Add(param4);
+                        SqlParameter param5 = new SqlParameter {
+                            ParameterName = "@id",
+                            Value = id,
+                            SqlDbType = SqlDbType.Int
+                        };
+                        command.Parameters.Add(param5);
                         connection.Open();
                         command.ExecuteNonQuery();
                         connection.Close();
