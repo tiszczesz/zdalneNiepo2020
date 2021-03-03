56a7c085d0120809698cd979c6dc387874a76185
pszczesz <pszczesz@gmail.com>
2021-03-03 08:31:55 +0100

    dd

--- szkola2020/2tiGim/cw11/cw11.php
+++ szkola2020/2tiGim/cw11/cw11.php
@@ -38,9 +38,25 @@
             return $result;
         }
     }//todo napisac klase DocImage(width,height) dziedziczaca z Document
+    class DocImage extends Document{
+        private int $width;
+        private int $height;
+        public function __construct(int $width,int $height,string $name="noname",int $size=0 )
+        {
+            parent::__construct($name,$size);
+            $this->width = $width;
+            $this->height = $height;
+        }
+        public function __toString():string
+        {
+           return parent::__toString()."<br>Szerokość obrazka: {$this->width} wysokość obrazka: {$this->height}<br>";
+        }
+    }
     $d = new DocText(["wewewe","rrrrr fffff ff","try ryryr yr yryr"]);
+    $im1 = new DocImage(300,600,"Ładny rodzinny obrazek",3000);
     var_dump($d);
     echo $d;
+    echo $im1;
     ?>
 </body>
 </html>
