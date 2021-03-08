4a3cec096b8339b998a6a16eba76ec3e55f443a6
Paweł Szczeszek <pszczesz@gmail.com>
2021-03-08 10:52:55 +0100

    dd

---
+++ webowe2tiGim/AdoNetCw2/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdoNetCw2.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Telefon { get; set; }
+        
+        [Display(Name = "Adres Email")]
+        public string Email { get; set; }
+    }
+}
