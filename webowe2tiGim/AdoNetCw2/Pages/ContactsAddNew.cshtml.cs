5792ded70f08daced5ba00e0305af376e4dcfcff
Paweł Szczeszek <pszczesz@gmail.com>
2021-03-08 10:37:59 +0100

    dd

---
+++ webowe2tiGim/AdoNetCw2/Pages/ContactsAddNew.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdoNetCw2.Pages
+{
+    public class ContactsAddNewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ webowe2tiGim/AdoNetCw2/sql/CreateDB.sql
@@ -0,0 +1,22 @@
+﻿
+--nazwa bazy "Gim_2021_Contacts"
+
+
+
+
+/****** Object: Table [dbo].[MyContacts] Script Date: 2021-03-08 10:28:17 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+use [Gim_2021_Contacts]
+Go
+
+CREATE TABLE [dbo].[MyContacts] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [Nazwisko] NVARCHAR (50) NOT NULL,
+    [Imie]     NVARCHAR (50) NULL,
+    [Telefon]  VARCHAR (20)  NULL,
+    [Email]    VARCHAR (50)  NULL
+);
