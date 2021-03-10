e5fd161b370783f1bb31ad7610453112cc910c35
pszczesz <pszczesz@gmail.com>
2021-03-10 10:24:22 +0100

    gg

--- webowe2tiSP/AdoNet_Posts/AdoNet_Posts/Pages/ListOfPosts.cshtml.cs
+++ webowe2tiSP/AdoNet_Posts/AdoNet_Posts/Pages/ListOfPosts.cshtml.cs
@@ -2,15 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AdoNet_Posts.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace AdoNet_Posts.Pages
 {
-    public class ListPOfostsModel : PageModel
+    
+    public class ListOfPostsModel : PageModel
     {
+        [BindProperty]
+        public List<Post> Posts { get; set; }
+        private IConfiguration configuration;
+        private string connection;
+
+        public ListOfPostsModel(IConfiguration configuration) {
+            this.configuration = configuration;
+            this.connection = this.configuration["ConnectionStrings:DefaultConnection"];
+        }
         public void OnGet()
         {
+
         }
     }
 }
