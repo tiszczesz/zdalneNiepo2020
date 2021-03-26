958f7f2c0fa805a300cabe6ba4dd805ce0974293
pszczesz <pszczesz@gmail.com>
2021-03-26 10:18:34 +0100

    dd

---
+++ zdalneJSGdow/cw18/cw18.js
@@ -0,0 +1,7 @@
+//cala stron jest biala i szukamy skarbu niewidocznego (opacity = 0)
+//probujemy go znamelzc  w wersji ambitniejszej na czas alobo click albo najechanie myszka odkrywa
+// skarb
+// rozpoczac prace poprzez zrobienie strony z divem o rozmiarze 100%
+// jak ktos przygotuje sama strone ze skarbem w losowym miejscu
+// to moze sie zglosic po ocene
+//we wtorek dokonczenie jak sami nie dacie rady
