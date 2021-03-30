275a3293501dee7553d078538ce68f23098037ee
pszczesz <pszczesz@gmail.com>
2021-03-30 08:59:23 +0200

    dd

--- zdalneJSGdow/cw18/cw18.js
+++ zdalneJSGdow/cw18/cw18.js
@@ -5,3 +5,22 @@
 // jak ktos przygotuje sama strone ze skarbem w losowym miejscu
 // to moze sie zglosic po ocene
 //we wtorek dokonczenie jak sami nie dacie rady
+
+document.addEventListener('DOMContentLoaded', function () {
+    let time = 0
+    setInterval(function(){
+        document.querySelector("#timer").innerHTML = "Czas: " +(++time)+" s";
+    },1000);
+  const widthScene = document.querySelector('#scene').offsetWidth;
+  const heightScene = document.querySelector('#scene').offsetHeight;
+  const treasure = document.querySelector('#treasure');
+  console.log(widthScene, heightScene);
+  const top = Math.floor(Math.random() * (heightScene - treasure.offsetHeight));
+  const left = Math.floor(Math.random() * (widthScene - treasure.offsetWidth));
+  console.log(top, left);
+  treasure.style.top = top + 'px';
+  treasure.style.left = left + 'px';
+  treasure.addEventListener("click",function(e){
+      e.target.style.opacity = 1;
+  });
+});
