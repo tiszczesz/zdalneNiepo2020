30eb11d3d8b5331db96bbfa9ca355ba01ff3166c
pszczesz <pszczesz@gmail.com>
2021-04-07 10:46:34 +0200

    dd

--- obiektowe 2tiSP/Operatory2_SP/Operatory2_SP/main.cpp
+++ obiektowe 2tiSP/Operatory2_SP/Operatory2_SP/main.cpp
@@ -24,12 +24,33 @@ void Ex1() {
 	scanf_s("%d", &wiek);
 	printf("Witaj %s %s masz wiek: %d", imie, nazwisko, wiek);
 }
+int NWDRek(int a, int b) {
+	if (b == 0) return a;
+	return NWDRek(b, a % b);
+}
+int NWDIter(int a, int b) {
+	int temp;
+	while(b!=0) {
+		temp = a;
+		a = b;
+		b = temp % b;
+	}
+	return a;
+}
 void Ex2() {
 	//todo nwd  45 60 --> 15 rekurencyjnie lub iteracyjnie bez cin cout (printf scanf_s)
+	printf("Podaj a: ");
+	int a;
+	scanf_s("%d", &a);
+	printf("Podaj b: ");
+	int b;
+	scanf_s("%d", &b);
+	printf("NWDRek(%d,%d) = %d\n", a, b, NWDRek(a, b));
+	printf("NWDIter(%d,%d) = %d\n", a, b, NWDIter(a, b));
 }
 int main() {
-	Ex1();
-
+	//Ex1();
+	Ex2();
 
 	
 	return 0;
