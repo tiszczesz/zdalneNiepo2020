e024a75f95680472c6b97d0759d50fc24ae3c48b
pszczesz <pszczesz@gmail.com>
2021-04-13 10:13:26 +0200

    vv

--- zdalneJSGdow/cw20/cw20.js
+++ zdalneJSGdow/cw20/cw20.js
@@ -1,30 +1,34 @@
-document.addEventListener("DOMContentLoaded",function(){
-    const ages = document.querySelectorAll(".age");
-   // console.log(ages);
-    for(let a of ages){
-        //console.log(a.parentNode);
-        if(a.innerHTML<18){
-            a.parentNode.style.backgroundColor = "red";
-        }
-        //todo wiersze w ktorych sa osoby o nazwisku krotszym niz 5 znakow maja miec kolor tekstu żółty
-        
+document.addEventListener('DOMContentLoaded', function () {
+  const ages = document.querySelectorAll('.age');
+  // console.log(ages);
+  for (let a of ages) {
+    //console.log(a.parentNode);
+    if (a.innerHTML < 18) {
+      a.parentNode.style.backgroundColor = 'red';
     }
-    const lastnames = document.querySelectorAll(".lastname");
-   // console.log(lastnames);
-    lastnames.forEach(function(v){
-        console.log(v);
-        if(v.innerHTML.trim().length<5){
-            v.parentNode.style.color = "orange";
-        }
-    })
-    const trs = document.querySelectorAll("tr");
-    for(let elem of trs){
-        elem.addEventListener("dblclick",function(e){
-            console.log(e.target.parentNode.parentNode.children);
-           e.target.parentNode.parentNode.removeChild(e.target.parentNode);
-        });
+    //todo wiersze w ktorych sa osoby o nazwisku krotszym niz 5 znakow maja miec kolor tekstu żółty
+  }
+  const lastnames = document.querySelectorAll('.lastname');
+  // console.log(lastnames);
+  lastnames.forEach(function (v) {
+    console.log(v);
+    if (v.innerHTML.trim().length < 5) {
+      v.parentNode.style.color = 'orange';
     }
-      //todo zbior paragrafów w divie i klikniecie podwójne usuwa klikniety paragraf
-
-
+  });
+  const trs = document.querySelectorAll('tr');
+  for (let elem of trs) {
+    elem.addEventListener('dblclick', function (e) {
+  //    console.log(e.target.parentNode.parentNode.children);
+      e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+    });
+  }
+  //todo zbior paragrafów w divie i klikniecie podwójne usuwa klikniety paragraf
+  const pars = document.querySelectorAll("#container p");
+  console.log(pars);
+  pars.forEach(function(elem){
+      elem.addEventListener('dblclick',function(e){
+          //alert(e.target.innerHTML);
+      });
+  });
 });
