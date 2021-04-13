4dd1950e144942d67bc7eee5ed6fc57511e4b6ef
pszczesz <pszczesz@gmail.com>
2021-04-13 10:45:08 +0200

    dd

--- zdalneJSGdow/cw20/cw20.js
+++ zdalneJSGdow/cw20/cw20.js
@@ -19,16 +19,36 @@ document.addEventListener('DOMContentLoaded', function () {
   const trs = document.querySelectorAll('tr');
   for (let elem of trs) {
     elem.addEventListener('dblclick', function (e) {
-  //    console.log(e.target.parentNode.parentNode.children);
+      //    console.log(e.target.parentNode.parentNode.children);
       e.target.parentNode.parentNode.removeChild(e.target.parentNode);
     });
   }
   //todo zbior paragrafów w divie i klikniecie podwójne usuwa klikniety paragraf
-  const pars = document.querySelectorAll("#container p");
+  const pars = document.querySelectorAll('#container p');
   console.log(pars);
-  pars.forEach(function(elem){
-      elem.addEventListener('dblclick',function(e){
-          //alert(e.target.innerHTML);
-      });
+  let newElem = document.createElement("p");
+  newElem.innerHTML = "To jest zupełnie nowy element";
+  newElem.addEventListener("contextmenu",function(e){e.preventDefault()});
+
+  let lista = document.createElement("ol");
+  for(let i=0;i<10;i++){
+      let elemListy = document.createElement("li");
+      elemListy.innerHTML = "Element listy numer: "+i;
+      lista.appendChild(elemListy);
+  }
+  document.body.appendChild(lista);
+  pars.forEach(function (elem) {
+    elem.addEventListener('dblclick', function (e) {
+      //alert(e.target.innerHTML);
+      document.querySelector('#container').removeChild(e.target);
+    });
+    elem.addEventListener("mousedown",function(e){
+        console.log(e);
+        if(e.buttons===2){
+            e.target.replaceWith(newElem);
+        }
+    });
+    elem.addEventListener("contextmenu",function(e){e.preventDefault();});
+
   });
 });
