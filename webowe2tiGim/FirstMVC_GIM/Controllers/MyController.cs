07a698f4f265585eea558b1c8d0ab74671fbce09
pszczesz <pszczesz@gmail.com>
2021-04-14 10:24:35 +0200

    dd

---
+++ webowe2tiGim/FirstMVC_GIM/Controllers/MyController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVC_GIM.Controllers
+{
+    public class MyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyAction(int? id) {
+            return View();
+        }
+    }
+}
