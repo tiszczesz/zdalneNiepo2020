d7c83bcaa760d3568d9ed8210594536744e934e1
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-05-05 14:04:37 +0200

    dd
    dd

---
+++ kurs ASP/main.kt
@@ -0,0 +1,18 @@
+
+package com.moj.pszczesz
+
+class Car(val type:String, val model:Int,val price:Double,val milesDrive:Int, val owner:String){
+    init{
+        println("Oject class is created")
+    }
+    override fun toString():String {
+        return "To auto $type model: $model"
+    }
+}
+fun main(){
+    val car = Car("BMW", 2015, 10000.000, 100, "pszczesz")
+    
+    val car2 = Car("BMWzzzz", 2015, 10000.000, 100, "pszczesz")
+    println(car2)
+
+}
