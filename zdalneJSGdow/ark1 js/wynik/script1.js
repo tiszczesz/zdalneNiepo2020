4844b91d3576fc17890796219023477de8accc41
pszczesz <pszczesz@gmail.com>
2021-05-07 08:49:16 +0200

    ff

--- zdalneJSGdow/ark1 js/wynik/script1.js
+++ zdalneJSGdow/ark1 js/wynik/script1.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded",function(){
     document.querySelector("#zamow").addEventListener("click",function(){
+        //document.querySelector("#zamow").onclick = function(){....}
         let ksztalt = document.querySelector("#ksztalt").value;
         let wybranyKsztlt = "inny kształt";
         switch(ksztalt){
@@ -10,4 +11,16 @@ document.addEventListener("DOMContentLoaded",function(){
         }
         document.querySelector("#wynik").innerHTML = "Zamówiłeś żelka: "+wybranyKsztlt;
     });
+
+    document.querySelector("#kolor").addEventListener("click",function(e){
+        let red = parseInt(document.querySelector("#red").value);
+        let green = parseInt(document.querySelector("#green").value);
+        let blue = parseInt(document.querySelector("#blue").value);
+        if(isNaN(red) || isNaN(green) || isNaN(blue)){
+            alert("Błędne wartości kolorów!!");
+            return;
+        }
+        e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    });
+
 });
