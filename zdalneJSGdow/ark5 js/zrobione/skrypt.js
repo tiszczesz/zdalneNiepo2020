a5c9dcbd76af95bcd2b23c8d536c87b61452ee21
tiszczesz <tiszczesz@users.noreply.github.com>
2021-05-21 10:15:11 +0200

    ff

---
+++ zdalneJSGdow/ark5 js/zrobione/skrypt.js
@@ -0,0 +1,9 @@
+
+document.querySelector("#oblicz").addEventListener("click",function(){
+    //.alert("ffff");
+
+    let ile = document.querySelector("#ilosc").value;
+    let poprawiany = document.querySelector("#poprawiny").checked;
+    console.log(poprawiany);
+    alert(ile);
+});
