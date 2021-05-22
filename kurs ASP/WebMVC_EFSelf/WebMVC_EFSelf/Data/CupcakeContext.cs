4e31c371bab7544e3baa7d1dbc7d9c7665fdedc2
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-05-22 12:45:42 +0200

    Update CupcakeContext.cs

--- kurs ASP/WebMVC_EFSelf/WebMVC_EFSelf/Data/CupcakeContext.cs
+++ kurs ASP/WebMVC_EFSelf/WebMVC_EFSelf/Data/CupcakeContext.cs
@@ -49,7 +49,54 @@ namespace WebMVC_EFSelf.Data
 
                 );
             modelBuilder.Entity<Cupcake>().HasData(
-
+                new Cupcake
+                {
+                    CupcakeId = 1,
+                    CupcakeType = CupcakeType.Birthday,
+                    Description = "Vanilla cupcake with coconut cream",
+                    GlutenFree = true,
+                    Price = 2.5,
+                    BakeryId = 1,
+                    ImageMimeType = "image/jpeg",
+                    ImageName = "birthday-cupcake.jpg",
+                    CaloricValue = 355
+                },
+                new Cupcake
+                {
+                    CupcakeId = 2,
+                    CupcakeType = CupcakeType.Chocolate,
+                    Description = "Chocolate cupcake with caramel filling and chocolate butter cream",
+                    GlutenFree = false,
+                    Price = 3.2,
+                    BakeryId = 2,
+                    ImageMimeType = "image/jpeg",
+                    ImageName = "chocolate-cupcake.jpg",
+                    CaloricValue = 195
+                },
+                new Cupcake
+                {
+                    CupcakeId = 3,
+                    CupcakeType = CupcakeType.Strawberry,
+                    Description = "Chocolate cupcake with straberry cream filling",
+                    GlutenFree = false,
+                    Price = 4,
+                    BakeryId = 3,
+                    ImageMimeType = "image/jpeg",
+                    ImageName = "pink-cupcake.jpg",
+                    CaloricValue = 295
+                },
+                new Cupcake
+                {
+                    CupcakeId = 4,
+                    CupcakeType = CupcakeType.Turquoise,
+                    Description = "Vanilla cupcake with butter cream",
+                    GlutenFree = true,
+                    Price = 1.5,
+                    BakeryId = 4,
+                    ImageMimeType = "image/jpeg",
+                    ImageName = "turquoise-cupcake.jpg",
+                    CaloricValue = 360
+                }
                 );
         }
     }
