88ba97890c3da5c7953b090ebbe356a015888d62
pszczesz <pszczesz@gmail.com>
2021-05-25 14:13:22 +0200

    ww

---
+++ webowe2tiSP/WebMVC_DB2_sp/WebMVC_DB2_sp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebMVC_DB2_sp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
