b4d862d758ea0a96908c33d353276db00a38844f
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-05-25 11:26:14 +0200

    sw

--- zdalneJSGdow/ark6 js/skrypt1.js
+++ zdalneJSGdow/ark6 js/skrypt1.js
@@ -1,5 +1,10 @@
+
+
 document.addEventListener("DOMContentLoaded", function () {
   console.log("ddd");
+  let img = document.querySelector("#ob1");
+  img.style.left = "200px";
+  img.style.top = "200px";
   document.querySelector("#ob1").addEventListener("mousedown", function (e) {
     console.log(e);
     switch (e.buttons) {
@@ -18,8 +23,34 @@ document.addEventListener("DOMContentLoaded", function () {
         break;
     }
   });
-  document.querySelector("#ob1").addEventListener("dblclick",function(e){
-      e.target.src = "p1.png";
+  document.body.addEventListener("keydown", function (e) {
+    console.log(e);
+    
+    
+    let left = parseInt(img.style.left);
+    let top = parseInt(img.style.top);
+    console.log(`left: ${left} top: ${top}`);
+    switch (e.key) {
+      case "w":
+        console.log("ruch do góry");
+        img.style.top = (top-10)+"px";
+        break;
+      case "s":
+        console.log("ruch na dół");
+        img.style.top = (top+10)+"px";
+        break;
+      case "a":
+        console.log("ruch w lewo");
+        img.style.left = (left-10)+"px";
+        break;
+      case "d":
+        console.log("ruch w prawo");
+        img.style.left = (left+10)+"px";
+        break;
+    }
+  });
+  document.querySelector("#ob1").addEventListener("dblclick", function (e) {
+    e.target.src = "p1.png";
   });
   document.querySelector("#ob1").addEventListener("contextmenu", function (e) {
     e.preventDefault();
