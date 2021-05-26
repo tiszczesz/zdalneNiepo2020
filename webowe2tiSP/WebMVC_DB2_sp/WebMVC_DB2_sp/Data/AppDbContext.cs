023ff7ee8d679328089d7cb866ff629b2e3608ee
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-05-26 10:35:38 +0200

    cc

---
+++ webowe2tiSP/WebMVC_DB2_sp/WebMVC_DB2_sp/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebMVC_DB2_sp.Models;
+
+namespace WebMVC_DB2_sp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) {
+        }
+
+        public DbSet<Animal> Animals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            int dd = 0;
+        }
+    }
+}
--- webowe2tiSP/WebMVC_DB2_sp/WebMVC_DB2_sp/Models/Animal.cs
+++ webowe2tiSP/WebMVC_DB2_sp/WebMVC_DB2_sp/Models/Animal.cs
@@ -11,8 +11,17 @@ namespace WebMVC_DB2_sp.Models
         [Key] 
         public int Id { get; set; }
 
+        [Display(Name = "Nazwa")]
+        [Required(ErrorMessage = "Podaj nazwę")]
         public string Name { get; set; }
 
+        [Display(Name = "Masa ciała")]
+        [Required(ErrorMessage = "Podaj masę")]
+        [Range(1,2000)]
         public int Weight { get; set; }
+
+        [Display(Name = "Opis")]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
     }
 }
