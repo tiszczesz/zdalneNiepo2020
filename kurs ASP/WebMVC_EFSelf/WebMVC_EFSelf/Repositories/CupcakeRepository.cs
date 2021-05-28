8854c0286af81b246e677d592d7bd2c6cd79df75
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-05-28 16:55:05 +0200

    rr

---
+++ kurs ASP/WebMVC_EFSelf/WebMVC_EFSelf/Repositories/CupcakeRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMVC_EFSelf.Models;
+
+namespace WebMVC_EFSelf.Repositories
+{
+    public class CupcakeRepository : ICupcakeRepository
+    {
+        public void CreateCupcake(Cupcake cupcake)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCupcake(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cupcake GetCupcakeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cupcake> GetCupcakes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Bakery> PopulateBakeriesDropDownList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
