5c3884fbaeedd6098342028244cbc73d463db371
pszczesz <pszczesz@gmail.com>
2021-05-28 10:46:16 +0200

    Update script1.js

--- zdalneJSGdow/ark7 js/zrobione/script1.js
+++ zdalneJSGdow/ark7 js/zrobione/script1.js
@@ -1,22 +1,25 @@
-let pswd = prompt("Podaj hasło:");
-if(pswd!="liczba"){
-    location.href = "index.html";
+let pswd = prompt('Podaj hasło:');
+if (pswd != 'liczba') {
+  location.href = 'index.html';
 }
 
-document.addEventListener("DOMContentLoaded",function(){
-    let x = parseInt(prompt("Podaj liczbę całkowitą"));
-    console.log(x);
-    if(isNaN(x)){
-        document.querySelector("#wynik").innerHTML = "Błędne dane";
-        return;
-    }
-    if(x>0){
-        document.querySelector("#wynik").innerHTML = "Liczba "+x+" jest dotatnia";
-    }else if(x<0){
-        document.querySelector("#wynik").innerHTML = "Liczba "+x+" jest ujemna";
-    }else if(x===0){
-        document.querySelector("#wynik").innerHTML = "Liczba "+x+" to zero";
-    }else{
-        document.querySelector("#wynik").innerHTML = "Błędne dane";
-    }
-});
+//document.addEventListener("DOMContentLoaded",function(){
+let x = parseInt(prompt('Podaj liczbę całkowitą'));
+console.log(x);
+if (isNaN(x)) {
+  document.querySelector('#wynik').innerHTML = 'Błędne dane';
+  // return;
+} else {
+  if (x > 0) {
+    document.querySelector('#wynik').innerHTML =
+      'Liczba ' + x + ' jest dotatnia';
+  } else if (x < 0) {
+    document.querySelector('#wynik').innerHTML = 'Liczba ' + x + ' jest ujemna';
+  } else if (x === 0) {
+    document.querySelector('#wynik').innerHTML = 'Liczba ' + x + ' to zero';
+  } else {
+    document.querySelector('#wynik').innerHTML = 'Błędne dane';
+  }
+}
+
+//});
