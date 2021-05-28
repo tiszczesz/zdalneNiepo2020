2d9a8b14878105995575fc024ab2591b6ce3bacf
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-05-28 19:12:36 +0200

    Update CupcakeRepository.cs

--- kurs ASP/WebMVC_EFSelf/WebMVC_EFSelf/Repositories/CupcakeRepository.cs
+++ kurs ASP/WebMVC_EFSelf/WebMVC_EFSelf/Repositories/CupcakeRepository.cs
@@ -1,26 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using WebMVC_EFSelf.Data;
 using WebMVC_EFSelf.Models;
 
-namespace WebMVC_EFSelf.Repositories
-{
+namespace WebMVC_EFSelf.Repositories {
     public class CupcakeRepository : ICupcakeRepository {
-        private CupcakeContext _context;
+        private readonly CupcakeContext _context;
 
         public CupcakeRepository(CupcakeContext context) {
             _context = context;
         }
-        public void CreateCupcake(Cupcake cupcake)
-        {
-            throw new NotImplementedException();
+
+        public void CreateCupcake(Cupcake cupcake) {
+            if (cupcake.PhotoAvatar != null && cupcake.PhotoAvatar.Length > 0) {
+                cupcake.ImageMimeType = cupcake.PhotoAvatar.ContentType;
+                cupcake.ImageName = Path.GetFileName(cupcake.PhotoAvatar.FileName);
+                using (var memoryStream = new MemoryStream()) {
+                    cupcake.PhotoAvatar.CopyTo(memoryStream);
+                    cupcake.PhotFile = memoryStream.ToArray();
+                }
+
+                _context.Add(cupcake);
+                _context.SaveChanges();
+            }
         }
 
-        public void DeleteCupcake(int id)
-        {
+        public void DeleteCupcake(int id) {
             throw new NotImplementedException();
         }
 
@@ -37,12 +45,11 @@ namespace WebMVC_EFSelf.Repositories
             var bakieriesQuery = from b in _context.Bakeries
                 orderby b.BakeryName
                 select b;
-           // var dd = _context.Bakeries.OrderBy(b => b.BakeryName);
+            // var dd = _context.Bakeries.OrderBy(b => b.BakeryName);
             return bakieriesQuery;
         }
 
-        public void SaveChanges()
-        {
+        public void SaveChanges() {
             throw new NotImplementedException();
         }
     }
