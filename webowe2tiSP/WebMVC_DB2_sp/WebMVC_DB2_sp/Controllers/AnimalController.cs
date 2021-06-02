352d7eaf061de50ddb5da517610030d336b57fbc
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-06-02 10:33:41 +0200

    ww

---
+++ webowe2tiSP/WebMVC_DB2_sp/WebMVC_DB2_sp/Controllers/AnimalController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMVC_DB2_sp.Data;
+using WebMVC_DB2_sp.Models;
+
+namespace WebMVC_DB2_sp.Controllers
+{
+    public class AnimalController : Controller {
+        private AppDbContext _db;
+
+        public AnimalController(AppDbContext db) {
+            _db = db;
+        }
+        public IActionResult Index() {
+            List<Animal> animals = _db.Animals.ToList();
+            return View(animals);
+        }
+    }
+}
