5c840828a2f25b786d07c9568a3e370529433eb3
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-06-07 11:17:20 +0200

    dd

---
+++ webowe2tiGim/WebMVC_selfDB/WebMVC_selfDB/Data/ProductDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebMVC_selfDB.Models;
+
+namespace WebMVC_selfDB.Data {
+    public class ProductDbContext : DbContext {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options)
+            : base(options) {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>().HasData(
+                new Product {
+                    Id = 1,
+                    Name = "chleb",
+                    Price = 2.99M,
+                    Description = "Chrupiący chlebek na śniadanko"
+                },
+                new Product {
+                    Id = 2,
+                    Name = "bułka",
+                    Price = 0.99M,
+                    Description = "Chrupiący bułka na śniadanko"
+                },
+                new Product {
+                    Id = 3,
+                    Name = "masło",
+                    Price = 4.99M,
+                    Description = "Świeże masło prawdziwe"
+                },
+                new Product {
+                    Id = 4,
+                    Name = "śmietana 18%",
+                    Price = 5.66M,
+                    Description = "Kubek śmietany pożywnej"
+                }
+            );
+        }
+    }
+}
---
+++ webowe2tiGim/WebMVC_selfDB/WebMVC_selfDB/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVC_selfDB.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name ="Produkt")]
+        [MaxLength(50,ErrorMessage = "Maksymalnie 50 znaków")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Cena")]
+        [Required]
+        [Range(0.00,Double.MaxValue,ErrorMessage = "Cena nie może być ujemna")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Opis")]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+    }
+}
