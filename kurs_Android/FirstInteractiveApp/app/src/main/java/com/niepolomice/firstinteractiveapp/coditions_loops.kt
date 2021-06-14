dae0364a3327d9c55391426de603d2fa4be8c2c2
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-06-14 13:23:14 +0200

    time

--- kurs_Android/FirstInteractiveApp/app/src/main/java/com/niepolomice/firstinteractiveapp/coditions_loops.kt
+++ kurs_Android/FirstInteractiveApp/app/src/main/java/com/niepolomice/firstinteractiveapp/coditions_loops.kt
@@ -1,5 +1,10 @@
 package com.niepolomice.firstinteractiveapp
 
+import android.os.Build
+import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
+
 fun main(){
     for (i in 1..20){
         print("$i ")
@@ -15,4 +20,24 @@ fun main(){
     for (i in 1..limit){
         print(if(i%podzielnik==0) "$i " else "")
     }
+    diffDays()
+}
+fun diffDays() {
+  //  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        // parse the date with a suitable formatter
+        val from = LocalDate.parse("01062021", DateTimeFormatter.ofPattern("ddMMyyyy"))
+        // get today's date
+        val today = LocalDate.now()
+        // calculate the period between those two
+        var period = Period.between(from, today)
+        // and print it in a human-readable way
+        println(
+            "The difference between " + from.format(DateTimeFormatter.ISO_LOCAL_DATE)
+                    + " and " + today.format(DateTimeFormatter.ISO_LOCAL_DATE) + " is "
+                    + period.years + " years, " + period.months + " months and "
+                    + period.getDays() + " days"
+        )
+//    }else{
+//        print("Nie")
+//    }
 }
