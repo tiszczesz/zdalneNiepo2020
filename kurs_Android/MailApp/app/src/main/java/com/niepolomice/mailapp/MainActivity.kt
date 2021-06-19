17ce4d8a773d0b9353208fb4bc0fe6a0a75ad1e2
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-06-19 14:09:13 +0200

    ff

--- kurs_Android/MailApp/app/src/main/java/com/niepolomice/mailapp/MainActivity.kt
+++ kurs_Android/MailApp/app/src/main/java/com/niepolomice/mailapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.niepolomice.mailapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val emailTo = findViewById<EditText>(R.id.emailToEditText)
+        var buttonSend = findViewById<Button>(R.id.sendButton)
+        val topicEditText = findViewById<EditText>(R.id.topicEditText)
+        buttonSend.setOnClickListener {
+            val toEmailText = emailTo.text.toString().trim()
+            if(toEmailText!=""){
+                Toast.makeText(this@MainActivity,
+                    "Wysłano email do: ${toEmailText}",Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this@MainActivity,
+                    "Brak adresu",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
