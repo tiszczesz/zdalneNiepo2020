f25a1aeea32c8882174982842083076f658eb02e
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-06-26 13:44:07 +0200

    Update SecondActivity.kt

--- kurs_Android/GenerateActivity/app/src/main/java/com/niepolomice/generateactivity/SecondActivity.kt
+++ kurs_Android/GenerateActivity/app/src/main/java/com/niepolomice/generateactivity/SecondActivity.kt
@@ -2,10 +2,13 @@ package com.niepolomice.generateactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.niepolomice.generateactivity.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var bind:ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        bind = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(bind.root)
     }
 }
