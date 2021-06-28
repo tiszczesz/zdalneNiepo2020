1e64472d896238bf30b9ba5852681b146ce984e2
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-06-28 09:35:18 +0200

    dd

--- kurs_Android/MyAppActivityWithResult/app/src/main/java/com/niepolomice/myappactivitywithresult/MainActivity.kt
+++ kurs_Android/MyAppActivityWithResult/app/src/main/java/com/niepolomice/myappactivitywithresult/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bind: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
     }
 }
