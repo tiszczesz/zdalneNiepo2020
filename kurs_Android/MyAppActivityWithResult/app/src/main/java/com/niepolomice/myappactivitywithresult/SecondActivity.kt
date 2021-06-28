e0ac0d68278551c9617289855eae81876039a111
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-06-28 09:31:52 +0200

    dd

--- kurs_Android/MyAppActivityWithResult/app/src/main/java/com/niepolomice/myappactivitywithresult/SecondActivity.kt
+++ kurs_Android/MyAppActivityWithResult/app/src/main/java/com/niepolomice/myappactivitywithresult/SecondActivity.kt
@@ -2,12 +2,10 @@ package com.niepolomice.myappactivitywithresult
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.niepolomice.myappactivitywithresult.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var bind: ActivityMainBinding
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
     }
 }
