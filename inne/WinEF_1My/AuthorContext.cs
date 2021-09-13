179f0c843d312b3721ddbddbfc0cf536dbbf4905
Paweł Szczeszek <pszczesz@gmail.com>
2021-09-13 19:35:44 +0200

    gg

---
+++ inne/WinEF_1My/AuthorContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WinEF_1My
+{
+    public class AuthorContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AuthorDbMy1;");
+        }
+
+        public DbSet<Author> Authors { get; set; }
+    }
+}
---
+++ inne/WinEF_1My/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinEF_1My
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var context = new AuthorContext())
+            {
+                context.Database.EnsureCreated();
+                var authors = new List<Author>
+                {
+                    new Author { FirstName="Carson", LastName="Alexander", BirthDate = DateTime.Parse("1985-09-01")},
+                    new Author { FirstName="Meredith", LastName="Alonso", BirthDate = DateTime.Parse("1970-09-01")},
+                    new Author { FirstName="Arturo", LastName="Anand", BirthDate = DateTime.Parse("1963-09-01")},
+                    new Author { FirstName="Gytis", LastName="Barzdukas", BirthDate = DateTime.Parse("1988-09-01")},
+                    new Author { FirstName="Yan", LastName="Li", BirthDate = DateTime.Parse("2000-09-01")},
+                };
+
+                context.Authors.AddRange(authors);
+                context.SaveChanges();
+
+                dataGridView1.DataSource = authors;
+            }
+        }
+    }
+}
