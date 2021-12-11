d8f9f1338d802c2b767449c6a331f63b36cce120
pszczesz <pszczesz@gmail.com>
2021-12-11 15:37:45 +0100

    Update cw6.js

--- JS_kurs_2021_11/cw4/cw6.js
+++ JS_kurs_2021_11/cw4/cw6.js
@@ -2,13 +2,17 @@ let a = [];
 fetch("https://jsonplaceholder.typicode.com/users")
                .then(response=>response.json())
                .then((json)=>{
-                    a = json;
-                    console.log(a);
-                    a = json;
+                    a = json
+                    console.log('czyste fetch: ',a);
+                    //a = json;
                     //document.querySelector("#root").innerHTML = data[0].title;
                });
- setTimeout(()=>{console.log(a);},2000);              
-
+// setTimeout(()=>{console.log(a);},2000);              
+(async ()=>{
+      a = await fetch("https://jsonplaceholder.typicode.com/users")
+     .then(response=>response.json());
+     console.log('async await: ' ,a);
+})();
 //drugi sposób
 
 
