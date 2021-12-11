6ff94f8c161fba8b915a6aa74ad426770b0219b3
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-12-11 13:03:35 +0100

    gg

--- JS_kurs_2021_11/cw4/cw4.js
+++ JS_kurs_2021_11/cw4/cw4.js
@@ -10,3 +10,52 @@ function canDrive(user) {
 var eryk = { name: "Eryk", ege: 34 };
 var tom = { name: "Tom", age: 22 };
 canDrive(tom);
+var books = [
+    {
+        title: "ala ma kota", author: "Antoni Małecki", price: 34.66,
+        editor: { name: "ASP Editors", address: "Młyńska 34", dateOf: "12-12-2002" }
+    },
+    {
+        title: "rrrr", author: "Antoni Małecki", price: 34.66,
+        editor: { name: "ASP Editors", address: "Młyńska 34", dateOf: "04-11-2002" }
+    },
+    {
+        title: "ttttt", author: "Antoni Małecki", price: 34.66,
+        editor: { name: "ASP Editors", address: "Młyńska 34", dateOf: "09-12-2012" }
+    },
+];
+console.log(books);
+var ShowEditor = function (editor) {
+    var ul = document.createElement("ul");
+    for (var item in editor) {
+        var li = document.createElement("li");
+        var txt = document.createTextNode("".concat(item, ": ").concat(editor[item]));
+        li.appendChild(txt);
+        ul.appendChild(li);
+    }
+    return ul;
+};
+var ShowAllBooks = function (books) {
+    var table = document.createElement("table");
+    table.innerHTML = "<tr><th>Tytuł</th><th>Autor</th><th>cena</th><th>Wydawca</th></tr>";
+    for (var _i = 0, books_1 = books; _i < books_1.length; _i++) {
+        var b = books_1[_i];
+        var tr = document.createElement("tr");
+        generRow(tr, b.title);
+        generRow(tr, b.author);
+        generRow(tr, b.price.toString());
+        var td4 = document.createElement("td");
+        td4.appendChild(ShowEditor(b.editor));
+        tr.appendChild(td4);
+        table.appendChild(tr);
+    }
+    return table;
+};
+function generRow(tr, b) {
+    var td = document.createElement("td");
+    td.appendChild(document.createTextNode(b));
+    tr.appendChild(td);
+    return tr;
+}
+console.log(ShowEditor);
+document.querySelector("#root").appendChild(ShowAllBooks(books));
--- JS_kurs_2021_11/cw4/cw4.ts
+++ JS_kurs_2021_11/cw4/cw4.ts
@@ -1,40 +1,79 @@
-import { EncodeIntoResult } from "util";
 
-function canDrive(user:User){
-    console.log("użytkownik: ",user.name);
-    if(user.age >= 16){
-        console.log("może jeździć samochodem");        
-    }else{
-        console.log("nie może jeździć samochodem");        
-    }    
+function canDrive(user: User): void {
+    console.log("użytkownik: ", user.name);
+    if (user.age >= 16) {
+        console.log("może jeździć samochodem");
+    } else {
+        console.log("nie może jeździć samochodem");
+    }
 }
 
-const eryk = {name:"Eryk",ege:34};
-const tom = {name:"Tom",age:22};
+const eryk = { name: "Eryk", ege: 34 };
+const tom = { name: "Tom", age: 22 };
 canDrive(tom);
 
-interface User{
-    name:string;
-    age:number;
+interface User {
+    name: string;
+    age: number;
 }
-const books:Array<Book> = [
-    {title:"ala ma kota",author:"Antoni Małecki",price:34.66,
-          editor:{name:"ASP Editors",address:"Młyńska 34",dateOf:new Date("12-12-2002")}},
-          {title:"rrrr",author:"Antoni Małecki",price:34.66,
-          editor:{name:"ASP Editors",address:"Młyńska 34",dateOf:new Date("04-11-2002")}},
-          {title:"ttttt",author:"Antoni Małecki",price:34.66,
-          editor:{name:"ASP Editors",address:"Młyńska 34",dateOf:new Date("09-12-2012")}},     
+const books: Array<Book> = [
+    {
+        title: "ala ma kota", author: "Antoni Małecki", price: 34.66,
+        editor: { name: "ASP Editors", address: "Młyńska 34", dateOf: "12-12-2002" }
+    },
+    {
+        title: "rrrr", author: "Antoni Małecki", price: 34.66,
+        editor: { name: "ASP Editors", address: "Młyńska 34", dateOf: "04-11-2002" }
+    },
+    {
+        title: "ttttt", author: "Antoni Małecki", price: 34.66,
+        editor: { name: "ASP Editors", address: "Młyńska 34", dateOf: "09-12-2012" }
+    },
 ];
+console.log(books);
 
+const ShowEditor = (editor: Editor): HTMLUListElement => {
+    let ul = document.createElement("ul");
+    for (let item in editor) {
+        let li = document.createElement("li");
+        let txt = document.createTextNode(`${item}: ${editor[item]}`);
+        li.appendChild(txt);
+        ul.appendChild(li);
+    }
+    return ul;
+};
+const ShowAllBooks = (books: Array<Book>) => {
+    let table = document.createElement("table");
+    table.innerHTML = "<tr><th>Tytuł</th><th>Autor</th><th>cena</th><th>Wydawca</th></tr>";
+    for (let b of books) {
+        let tr = document.createElement("tr");
+        generRow(tr, b.title);
+        generRow(tr, b.author);
+        generRow(tr, b.price.toString());
+        let td4 = document.createElement("td");
+        td4.appendChild(ShowEditor(b.editor));
+        tr.appendChild(td4);
+        table.appendChild(tr);
+    }
+    return table;
+}
 
-interface Book{
-    title:string;
-    author:string;
-    price:number;
-    editor:Editor;
+function generRow(tr: HTMLTableRowElement, b: string) {
+    let td = document.createElement("td");
+    td.appendChild(document.createTextNode(b));
+    tr.appendChild(td);
+    return tr;
 }
-interface Editor{
-    name:string;
-    address:string;
-    dateOf:Date;
+interface Book {
+    title: string;
+    author: string;
+    price: number;
+    editor: Editor;
 }
+interface Editor {
+    name: string;
+    address: string;
+    dateOf: string;
+}
+console.log(ShowEditor);
+document.querySelector("#root").appendChild(ShowAllBooks(books));
