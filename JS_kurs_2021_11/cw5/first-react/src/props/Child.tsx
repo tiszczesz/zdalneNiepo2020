91a99e9a6e021cee3b5b9681f3f4ee19c3724a80
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2021-12-17 18:44:05 +0100

    ff

--- JS_kurs_2021_11/cw5/first-react/src/props/Child.tsx
+++ JS_kurs_2021_11/cw5/first-react/src/props/Child.tsx
@@ -1,10 +1,20 @@
 interface ChildProps {
     cmpName: string;
+    onClick: ()=>void;
 }
 
 
-export const Child = ({cmpName}:ChildProps) => {
+export const Child = ({cmpName,onClick}:ChildProps) => {
     return <div>
-        Witam z komponentu dziecka o nazwie {cmpName}
+        Witam z komponentu dziecka o nazwie {cmpName} <br />
+        <button onClick={onClick}>Przycisk</button>
+    </div>
+}
+
+export const ChildAsFC: React.FC<ChildProps> = ({onClick,cmpName,children}) => {
+    return <div className="first">
+        Komponent FC {cmpName}
+        {children} <br />
+        <button onClick={onClick}>Przycisk</button>
     </div>
 }
--- JS_kurs_2021_11/cw5/first-react/src/props/Parent.tsx
+++ JS_kurs_2021_11/cw5/first-react/src/props/Parent.tsx
@@ -1,13 +1,22 @@
-import { Child } from "./Child";
+import { log } from "console";
+import { Child, ChildAsFC } from "./Child";
 import { Child2 } from "./Child2";
 
 const Parent = () => {
     return (
-        <div className="first"> 
-        <Child cmpName="Jancio" />
-        <Child2 dateNow={new Date()} />
+        <div className="first">
+            <Child
+                cmpName="Jancio"
+                onClick={() => { console.log("kliknięto Child") }}
+            />
+            <Child2 dateNow={new Date()} />
+            <ChildAsFC
+                cmpName="Święta prawie"
+                onClick={()  => {
+                        console.log("kliknięto ChildAsFC: ");
+                }}>To jest inneText</ChildAsFC>
         </div>
-       
+
     );
 }
 
